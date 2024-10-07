d1c4adba39ba65c04bc2965e3f239615dd7a7a9d
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2024-10-07 15:11:04 +0200

    Menu finsished

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ void setup()
 
 void loop() 
 {
-  blink();
-  menu();
+  blink();  //function for light to blink
+  menu();   //menu function
 }
 
 //sum function
@@ -26,53 +26,71 @@ int sumNum(int x, int y)
 //turns led on and off
 void blink()
 {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on 
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off 
   delay(1000);                      // wait for a second
 }
 
-//function to do either sum or multi
+//function to do either add or multiply
 void menu()
 {
   int userAnswer=0;
-  Serial.print("1=Sum calculation\t2=Multiplication:\n");
+  Serial.print("1=Sum calculation\t2=Multiplication:\n"); //asks user what they will do
   while(Serial.available() == 0);
     userAnswer = Serial.parseInt();
-  if(userAnswer == 1)
+
+  if(userAnswer == 1)   //1 = Summing 
   {
-    int num1=0;
+    int num1=0;   
     int num2=0;
-    Serial.print("Enter digit 1:\n");
+
+    Serial.print("Enter digit 1:\n");   //user enters first digit
     while(Serial.available() == 0);
       num1 = Serial.parseInt();
-    Serial.print("Enter digit 2:\n");
+
+    Serial.print("Enter digit 2:\n");   //user enters second digit
     while(Serial.available() == 0);
       num2 = Serial.parseInt();
+
     //gets the sum and displays it
-    int sum = sumNum(num1, num2);
-    Serial.print("Sum of ");
+    int sum = sumNum(num1, num2);   //calls the function to sum
+
+    Serial.print("Sum of ");      //prints "Sum of num1 + num2 = answer"
     Serial.print(num1);
     Serial.print(" + ");
     Serial.print(num2);
     Serial.print(" = ");
     Serial.print(sum);
+    Serial.print("\n");
 
   } 
-  else if(userAnswer == 2)
+  else if(userAnswer == 2)  //multiply
   {
     int num1=0;
     int num2=0;
-    Serial.print("Enter digit 1:\n");
+    Serial.print("Enter digit 1:\n");   //user enters first digit
     while(Serial.available() == 0);
       num1 = Serial.parseInt();
-    Serial.print("Enter digit 2:\n");
+
+    Serial.print("Enter digit 2:\n");   //user enters second digit
     while(Serial.available() == 0);
       num2 = Serial.parseInt();
 
-    //add function to multiply
+    //TODO
+    //add function to multiply, with int value of answer called sum. Like in sum
+
+
+    //prints "Multiply of num1 * num2 = answer"
+    Serial.print("Sum of ");      
+    Serial.print(num1);
+    Serial.print(" * ");
+    Serial.print(num2);
+    Serial.print(" = ");
+    //Serial.print(sum);
+    Serial.print("\n");
   }
-  else
+  else  //displays error if the user didnt enter 1 or 2
   {
     Serial.print("ERROR! Type either 1 or 2.\n");
   }
